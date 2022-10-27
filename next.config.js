20fdf97e023f2f480377798ab1d541c6174e9571
Andi Basha <54249913+Andi-Basha@users.noreply.github.com>
2022-10-28 01:44:23 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   images:{
     domains: ['res.cloudinary.com']
   },
   swcMinify: true,
-  ignoreDuringBuilds: true,
 }
 
 module.exports = nextConfig
